284dac522b14b051f91867d8f94c63b682ce7be4
rociodepoli <rociodepoli69@gmail.com>
2023-03-29 20:09:03 -0300

    netlify forms

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -27,7 +27,7 @@ export default function Contact(){
     return (
         <div className={styles.container}>
         <form
-          netlify
+          data-netlify='true'
           name="contact"
           onSubmit={(e)=> handleSubmit(e)}
           >
